53c61f212c002f69f4867bab29234343eb5939f5
Julio Duarte <73315417+maoiki@users.noreply.github.com>
2022-06-01 01:12:25 -0300

    configs revisadas para deploy

--- web/src/lib/api.ts
+++ web/src/lib/api.ts
@@ -1,8 +1,5 @@
 import axios from 'axios';
 
-console.log(import.meta.env);
-console.log(import.meta.env.VITE_API_URL)
-
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
